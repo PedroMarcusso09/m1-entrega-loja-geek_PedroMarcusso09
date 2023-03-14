e2c2bf13dab1614c3d1268110918076e8e0958a8
Pedro Marcusso <100154939+PedroMarcusso09@users.noreply.github.com>
2023-03-14 11:27:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,129 +1,128 @@
 let itens = [
-    {
-      image: './assets/img/painting/clock.jpg',
-      name: 'Smartwatch',
-      price: 'R$100,00',
-      type: 'Painting'
-    },
-    {
-      image: './assets/img/painting/gamepad.jpg',
-      name: 'Controle Playstation',
-      price: 'R$100,00',
-      type: 'Painting'
-    },
-    {
-      image: './assets/img/painting/personagem.jpg',
-      name: 'Robot',
-      price: 'R$100,00',
-      type: 'Painting'
-    },
-    {
-      image: './assets/img/painting/clock.jpg',
-      name: 'Smartwatch',
-      price: 'R$100,00',
-      type: 'Painting'
-    },
-    {
-      image: './assets/img/painting/gamepad.jpg',
-      name: 'Controle Playstation',
-      price: 'R$100,00',
-      type: 'Painting'
-    },
-    {
-      image: './assets/img/painting/personagem.jpg',
-      name: 'Robot',
-      price: 'R$100,00',
-      type: 'Painting'
-    },
-    {
-      image: './assets/img/actions/animewoman.jpg',
-      name: 'Anime Woman',
-      price: 'R$100,00',
-      type: 'Action Figures'
-    },
-    {
-      image: './assets/img/actions/dragonballpersonagem.jpg',
-      name: 'Goku Super Saiyajin',
-      price: 'R$100,00',
-      type: 'Action Figures'
-    },
-    {
-      image: './assets/img/actions/starwarspersonagem.jpg',
-      name: 'Sr Yoda',
-      price: 'R$100,00',
-      type: 'Action Figures'
-    },
-    {
-      image: './assets/img/actions/animewoman.jpg',
-      name: 'Anime Woman',
-      price: 'R$100,00',
-      type: 'Action Figures'
-    },
-    {
-      image: './assets/img/actions/dragonballpersonagem.jpg',
-      name: 'Goku Super Saiyajin',
-      price: 'R$100,00',
-      type: 'Action Figures'
-    },
-    {
-      image: './assets/img/actions/starwarspersonagem.jpg',
-      name: 'Sr Yoda',
-      price: 'R$100,00',
-      type: 'Action Figures'
-    },
+  {
+    image: './assets/img/painting/clock.jpg',
+    name: 'Smartwatch',
+    price: 'R$100,00',
+    type: 'Painting'
+  },
+  {
+    image: './assets/img/painting/gamepad.jpg',
+    name: 'Controle Playstation',
+    price: 'R$100,00',
+    type: 'Painting'
+  },
+  {
+    image: './assets/img/painting/personagem.jpg',
+    name: 'Robot',
+    price: 'R$100,00',
+    type: 'Painting'
+  },
+  {
+    image: './assets/img/painting/clock.jpg',
+    name: 'Smartwatch',
+    price: 'R$100,00',
+    type: 'Painting'
+  },
+  {
+    image: './assets/img/painting/gamepad.jpg',
+    name: 'Controle Playstation',
+    price: 'R$100,00',
+    type: 'Painting'
+  },
+  {
+    image: './assets/img/painting/personagem.jpg',
+    name: 'Robot',
+    price: 'R$100,00',
+    type: 'Painting'
+  },
+  {
+    image: './assets/img/actions/animewoman.jpg',
+    name: 'Anime Woman',
+    price: 'R$100,00',
+    type: 'Action Figures'
+  },
+  {
+    image: './assets/img/actions/dragonballpersonagem.jpg',
+    name: 'Goku Super Saiyajin',
+    price: 'R$100,00',
+    type: 'Action Figures'
+  },
+  {
+    image: './assets/img/actions/starwarspersonagem.jpg',
+    name: 'Sr Yoda',
+    price: 'R$100,00',
+    type: 'Action Figures'
+  },
+  {
+    image: './assets/img/actions/animewoman.jpg',
+    name: 'Anime Woman',
+    price: 'R$100,00',
+    type: 'Action Figures'
+  },
+  {
+    image: './assets/img/actions/dragonballpersonagem.jpg',
+    name: 'Goku Super Saiyajin',
+    price: 'R$100,00',
+    type: 'Action Figures'
+  },
+  {
+    image: './assets/img/actions/starwarspersonagem.jpg',
+    name: 'Sr Yoda',
+    price: 'R$100,00',
+    type: 'Action Figures'
+  },
 ];
-    
-function separateItemsByType(itens) {
-let listPaintings = []
-let listFigures = []
+  
+function separarItensPorTipo(itens) {
+  let listPaintings = []
+  let listFigures = []
 
-for (let i = 0; i < itens.length; i++) {
+  for (let i = 0; i < itens.length; i++) {
     if (itens[i].type === 'Painting') {
-    listFigures.push(itens[i])
+      listFigures.push(itens[i])
     } else if (itens[i].type === 'Action Figures') {
-    listPaintings.push(itens[i])
+      listPaintings.push(itens[i])
     }
-}
-return {
+  }
+  return {
     listPaintings: listPaintings,
     listFigures: listFigures
+  }
 }
-
-}
-const { listPaintings, listFigures } = separateItemsByType(itens)
+const { listPaintings, listFigures } = separarItensPorTipo(itens)
 
 let secaoListFrames = document.querySelector('.painting_section')
 let secaoListFigures = document.querySelector('.figure_section')
 
-function createIten(object, type) {
-let createItenList = document.createElement('li')
-let createImage = document.createElement('img')
-let createName = document.createElement('h2')
-let createPrice = document.createElement('p')
+function criarItem(objeto, tipo) {
+let criarItemLista = document.createElement('li')
+let criarImagem = document.createElement('img')
+let criarNome = document.createElement('h2')
+let criarPreco = document.createElement('p')
 
-createItenList.classList.add('product')
-createImage.classList.add('product_image')
-createImage.src = object.image
-createName.classList.add('product_type')
-createName.innerText = object.name
-createPrice.classList.add('product_price')
-createPrice.innerText = object.price
+criarItemLista.classList.add('produto')
+criarImagem.classList.add('imagem_produto')
+criarImagem.src = objeto.image
+criarNome.classList.add('nome_produto')
+criarNome.innerText = objeto.name
+criarPreco.classList.add('valor_produto')
+criarPreco.innerText = objeto.price
 
-createItenList.appendChild(createImage)
-createItenList.appendChild(createName)
-createItenList.appendChild(createPrice)
+criarItemLista.appendChild(criarImagem)
+criarItemLista.appendChild(criarNome)
+criarItemLista.appendChild(criarPreco)
 
-if (type.toLowerCase() === 'painting') {
-secaoListFrames.appendChild(createItenList)
-} else if (type.toLowerCase() === 'action figures') {
-secaoListFigures.appendChild(createItenList)
-    }
+if (tipo.toLowerCase() === 'painting') {
+secaoListFrames.appendChild(criarItemLista)
+} else if (tipo.toLowerCase() === 'action figures') {
+secaoListFigures.appendChild(criarItemLista)
+  }
 }
 
 for (let i = 0; i < listFigures.length; i++) {
-createIten(listFigures[i], listFigures[i].type)
+criarItem(listFigures[i], listFigures[i].type)
 }
 
 for (let i = 0; i < listPaintings.length; i++) {
-createIten(listPaintings[i], listPaintings[i].type)
+criarItem(listPaintings[i], listPaintings[i].type)
 }
